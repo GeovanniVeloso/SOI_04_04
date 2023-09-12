7facc7f843c7d715a4f5db735347641fb1e13a25
GeovanniVeloso <141738879+GeovanniVeloso@users.noreply.github.com>
2023-09-11 21:56:23 -0300

    Add files via upload

--- SOI_04_04/src/controller/ThreadSapo.java
+++ SOI_04_04/src/controller/ThreadSapo.java
@@ -1,18 +1,22 @@
 package controller;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class ThreadSapo extends Thread {
 
 	private int sapo;
-	
+	private static AtomicInteger classificação = new AtomicInteger(1);
+	private int posição;
+
 	public ThreadSapo(int sapo) {
 		this.sapo = sapo;
 	}
-	
+
 	@Override
 	public void run() {
 		pulo();
 	}
-	
+
 	private void pulo() {
 		double d = 16;
 		double salto;
@@ -33,6 +37,19 @@ public class ThreadSapo extends Thread {
 			}
 		}
 		System.out.println("Sapo "+sapo+" finalizou a corrida!");
+
+		try {
+			sleep(5000);
+				posição = classificação.getAndIncrement();
+			}catch (Exception e) {
+			System.err.println(e);
+		}
+
+		System.out.println("O sapo "+sapo+" chegou em "+posição+"° lugar!");
 	}
+
+
+
+ 
 
 }
--- SOI_04_04/src/view/Principal.java
+++ SOI_04_04/src/view/Principal.java
@@ -5,12 +5,16 @@ import controller.ThreadSapo;
 public class Principal {
 
 	public static void main(String[] args) {
-		
+
 		for(int i = 1 ; i < 6 ; i++) {
 			Thread thSapo =new ThreadSapo(i);
 			thSapo.start();
 		}
 
+ 
+
 	}
+
+ 
 
 }
